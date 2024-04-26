7c4a26c2e743857f73d6100b70207faa153e15b1
angelayuan67 <155998049+angelayuan67@users.noreply.github.com>
2024-04-26 17:30:38 -0400

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -2,15 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 from scipy.signal import spectrogram
+import os
 
-
-mat = scipy.io.loadmat("S1_10_DF.otb%2B_decomp.mat_edited.mat")
+mat = scipy.io.loadmat("S1_20_DF.otb+_decomp.mat_edited.mat")
 
 signal = mat['signal']['data']
 
-data = signal.item()
+data = signal[0][0]    # 2d array
 
-for arr in data:
+save_folder = "data_1"
+
+for i, arr in enumerate(data):
     # Compute spectrogram for the current segment 'arr'
     f, t, Sxx = spectrogram(arr, fs=2000)  # Assuming a sampling frequency of 2000 Hz
     
@@ -22,7 +24,8 @@ for arr in data:
     plt.xlabel('Time [sec]')
     plt.colorbar(label='Power/Frequency [dB/Hz]')
     plt.tight_layout()
-    plt.show()
+    plt.savefig(os.path.join(save_folder, f"spectrogram_{i}.png"))
+    plt.close()
     # put into tensor
 
 # for arr in data:
@@ -39,3 +42,16 @@ for arr in data:
 # plt.tile('spectrogram')
 # plt.tight_layout()
 # plt.show()
+
+
+
+
+
+# mat.keys()
+# signal = mat['signal']
+# type(signal)
+# signal['path']
+
+
+
+
