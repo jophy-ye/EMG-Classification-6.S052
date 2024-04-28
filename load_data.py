ded8a335c6ae39e6a5b5462da763f503533b69c4
savnkk_main_machine_win <39103342+natask@users.noreply.github.com>
2024-04-28 07:40:17 -0400

    load_data: use mat73 over scipy to load mat files

--- load_data.py
+++ load_data.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy.signal import spectrogram
 import os
-
+import mat73
 
 class Signal:
     def __init__(self, filename): 
-        self.mat = scipy.io.loadmat(filename)
+        self.mat = mat73.loadmat(filename)
         self.signal = self.mat['signal']
+
     
     @property
     def data(self):
