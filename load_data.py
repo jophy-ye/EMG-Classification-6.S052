27a1865030415c200c67cc2c901c73576402fb6e
savnkk_main_machine_win <39103342+natask@users.noreply.github.com>
2024-05-01 11:44:10 -0400

    Capacity to load all data from first EMG dataset

--- load_data.py
+++ load_data.py
@@ -6,11 +6,16 @@ import os
 import mat73
 
 class Signal:
-    def __init__(self, filename): 
-        self.mat = mat73.loadmat(filename)
+    def __init__(self, filename):
+        try: 
+            self.mat = scipy.io.loadmat(filename) #104 files
+            self.signal = self.mat['signal']
+            self.mat['signal'] = {'data': self.signal["data"].item()}
+        except:
+            self.mat = mat73.loadmat(filename) #20 files with extra meta data
         self.signal = self.mat['signal']
 
     
     @property
     def data(self):
-        return self.signal['data'].item()
+        return self.signal['data']
