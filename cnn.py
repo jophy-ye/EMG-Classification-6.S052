5478e75b9281d666723b7c5fe0fe474c512ffd19
Jophy <jophyyjh@gmail.com>
2024-05-01 23:49:33 -0400

    Implemented training phase for task classification

--- cnn.py
+++ cnn.py
@@ -15,14 +15,14 @@ from tqdm import tqdm
 # X = 129
 # Y = 532 
 # as input layer
-class AudioClassifier (nn.Module):
+class AudioClassifier(nn.Module):
     # Build the model architecture
     def __init__(self):
         super().__init__()
         conv_layers = []
 
         # First Convolution Block with Relu and Batch Norm. Use Kaiming Initialization
-        self.conv1 = nn.Conv2d(2, 8, kernel_size=(5, 5), stride=(2, 2), padding=(2, 2))
+        self.conv1 = nn.Conv2d(2, 8, kernel_size=(9, 9), stride=(2, 2), padding=(4, 4))
         self.relu1 = nn.ReLU()
         self.bn1 = nn.BatchNorm2d(8)
         init.kaiming_normal_(self.conv1.weight, a=0.1)
@@ -30,7 +30,7 @@ class AudioClassifier (nn.Module):
         conv_layers += [self.conv1, self.relu1, self.bn1]
 
         # Second Convolution Block
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=(5, 5), stride=(2, 2), padding=(2, 2))
         self.relu2 = nn.ReLU()
         self.bn2 = nn.BatchNorm2d(16)
         init.kaiming_normal_(self.conv2.weight, a=0.1)
@@ -38,7 +38,7 @@ class AudioClassifier (nn.Module):
         conv_layers += [self.conv2, self.relu2, self.bn2]
 
         # Second Convolution Block
-        self.conv3 = nn.Conv2d(16, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=(5, 5), stride=(2, 2), padding=(2, 2))
         self.relu3 = nn.ReLU()
         self.bn3 = nn.BatchNorm2d(32)
         init.kaiming_normal_(self.conv3.weight, a=0.1)
@@ -46,7 +46,7 @@ class AudioClassifier (nn.Module):
         conv_layers += [self.conv3, self.relu3, self.bn3]
 
         # Second Convolution Block
-        self.conv4 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.conv4 = nn.Conv2d(32, 64, kernel_size=(5, 5), stride=(2, 2), padding=(2, 2))
         self.relu4 = nn.ReLU()
         self.bn4 = nn.BatchNorm2d(64)
         init.kaiming_normal_(self.conv4.weight, a=0.1)
@@ -55,7 +55,8 @@ class AudioClassifier (nn.Module):
 
         # Linear Classifier
         self.ap = nn.AdaptiveAvgPool2d(output_size=1)
-        self.lin = nn.Linear(in_features=64, out_features=10)
+        self.lin = nn.Linear(in_features=64, out_features=2)
+        self.activation = nn.Softmax()
 
         # Wrap the Convolutional Blocks
         self.conv = nn.Sequential(*conv_layers)
@@ -72,43 +73,43 @@ class AudioClassifier (nn.Module):
         x = x.view(x.shape[0], -1)
 
         # Linear layer
-        x = self.lin(x)
+        x = self.activation(self.lin(x))
 
         # Final output
         return x
 
 # Create the model and put it on the GPU if available
-myModel = AudioClassifier()
+model = AudioClassifier()
 device = (
     "cuda:0" if torch.cuda.is_available()
     else "mps" if torch.backends.mps.is_available()
     else "cpu"
 )
 device = torch.device(device)
-myModel = myModel.to(device)
+model = model.to(device)
 # Check that it is on Cuda
-next(myModel.parameters()).device
-# load a pretrained ResNet model
+next(model.parameters()).device
 
 train_dataset = EMGDataset('emg_dataset', mode="train", transform=data_transform['train'])
 val_dataset = EMGDataset('emg_dataset', mode="test", transform=data_transform['test'])
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(myModel.parameters(), lr=0.01)
+optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-input_tensors = []  # List of tensors, each one has dim (num_channels, height, width), length of list is 124 patients
-labels = []  # List of true labels
 
 # Concatenate the input tensors along the channel dimension
-num_epochs = 2000
+num_epochs = 800    # !!! main adjustable parameter !!!
+losses = []
 for epoch in tqdm(range(num_epochs)):
-    for ix in range(len(input_tensors)):
-        input_tensor = input_tensors[ix]
+    for ix in range(len(train_data)):
+        input_tensor, label = train_dataset[ix]
+        label = int(label == 'DF')  # convert to 0/1
         # Forward pass
-        output = myModel(input_tensor)
+        output = model(input_tensor)
 
         # Compute the loss
-        loss = criterion(output, labels[ix])
+        loss = criterion(output, label)
+        losses.append(loss)
 
         # Backward pass: Compute gradients
         optimizer.zero_grad()
@@ -117,3 +118,9 @@ for epoch in tqdm(range(num_epochs)):
         # Update weights
         optimizer.step()
 
+    if epoch % 20 == 0:
+        print(f'On epoch {epoch}: loss={losses[-1]}')
+    if epoch % 200 == 0:    # save periodically
+        torch.save(model.state_dict(), 'parameters_task.pt')
+
+torch.save(model.state_dict(), 'parameters_task.pt')
